a1b10cafa2838fa57fc3d34666c35173fc3c1cc0
limdp99 <47866457+limdp99@users.noreply.github.com>
2021-05-13 01:31:05 +0900

    Updates

---
+++ precache-manifest.fd375db554812fb8b317d75ca6a152a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85156654772adc365e1f5f37816b750f",
+    "url": "/redeem_hoa/index.html"
+  },
+  {
+    "revision": "85c993bfdd2c0fd3e319",
+    "url": "/redeem_hoa/static/css/main.c8326998.chunk.css"
+  },
+  {
+    "revision": "a74199fbe243160d88dd",
+    "url": "/redeem_hoa/static/js/2.6c6a24be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redeem_hoa/static/js/2.6c6a24be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c993bfdd2c0fd3e319",
+    "url": "/redeem_hoa/static/js/main.996f6430.chunk.js"
+  },
+  {
+    "revision": "9eb405949ce566ba057f",
+    "url": "/redeem_hoa/static/js/runtime-main.4117af50.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/redeem_if/precache-manifest.abc0124bbfd270cf75407015c633e3bb.js"
+  "/redeem_hoa/precache-manifest.fd375db554812fb8b317d75ca6a152a1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redeem_if/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redeem_hoa/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
